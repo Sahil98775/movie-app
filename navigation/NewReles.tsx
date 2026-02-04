1070c10cb35840443c93f9adb83c4dabe9a46834
Sahil98775 <sahil.sharma@chicmicstudios.in>
2026-02-04 18:10:27 +0530

    tenth commit

---
+++ navigation/NewReles.tsx
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+import NewRelease from '../Screens/NewRelease';
+import DetailsScreen from '../Screens/DetailsScreen';
+
+
+const Stack=createNativeStackNavigator();
+
+export default function NewRelsStack(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="Upcome" component={NewRelease} options={{headerShown:false}}/>
+            <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+
+    );
+
+}
+
+
+
+
