b4153dd4b02e16b4e97f8af8c623d6c51f5cc870
Sahil98775 <sahil.sharma@chicmicstudios.in>
2026-02-04 18:07:45 +0530

    seventh commit

---
+++ navigation/homeStack.tsx
@@ -0,0 +1,23 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+import HomeScreen from '../Screens/HomeScreen';
+import DetailsScreen from '../Screens/DetailsScreen';
+
+
+const Stack=createNativeStackNavigator()
+
+export default function HomeStack(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+            <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+
+    )
+
+}
+
+
+
+
+
+
